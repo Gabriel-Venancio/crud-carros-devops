66ae6829b9927d1e9f2dc9f234965eb433236634
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2023-04-09 20:57:31 -0300

    configurando o Papertrail

--- src/main/java/com/example/carroscruddevops/controller/CarController.java
+++ src/main/java/com/example/carroscruddevops/controller/CarController.java
@@ -2,6 +2,8 @@ package com.example.carroscruddevops.controller;
 
 import com.example.carroscruddevops.entities.Car;
 import com.example.carroscruddevops.service.CarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,18 +15,22 @@ import java.util.NoSuchElementException;
 @RestController
 public class CarController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CarController.class);
+
     @Autowired
     private CarService service;
 
     @GetMapping("/cars")
     public List<Car> list() {
-        return service.listAll();
+        LOG.info("List all req");
+         return service.listAll();
     }
 
     @GetMapping("cars/{id}")
     public ResponseEntity<Car> get(@PathVariable Long id) {
         try {
             Car car = service.getById(id);
+            LOG.info("List car by id:" + id);
             return new ResponseEntity<Car>(car, HttpStatus.OK);
         } catch (NoSuchElementException e) {
             return new ResponseEntity<Car>(HttpStatus.NOT_FOUND);
@@ -33,6 +39,7 @@ public class CarController {
 
     @PostMapping("/cars")
     public void add(@RequestBody Car car) {
+        LOG.info("Save car");
         service.save(car);
     }
 
@@ -40,6 +47,7 @@ public class CarController {
     public ResponseEntity<?> update(@RequestBody Car car, @PathVariable Long id) {
         try {
             Car existCar = service.getById(id);
+            LOG.info("Update car:" + id);
             service.save(car);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch(NoSuchElementException e) {
@@ -49,6 +57,8 @@ public class CarController {
 
     @DeleteMapping("/cars/{id}")
     public void delete(@PathVariable Long id) {
+
+        LOG.info("Delete car:" + id);
         service.delete(id);
     }
 }
